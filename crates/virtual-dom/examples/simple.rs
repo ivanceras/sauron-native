3e4a42057bade1358cd4a3eecc67e13ccf595930
Jovansonlee Cesar <ivanceras@gmail.com>
2019-04-04 21:43:07 +0800

    Prefer usage of module in edition2018 style

--- crates/virtual-dom/examples/simple.rs
+++ crates/virtual-dom/examples/simple.rs
@@ -1,54 +1,40 @@
+use virtual_dom::builder::*;
 use virtual_dom::diff;
-use virtual_dom::{Callback, Element, Node, Text, Value};
-
-use std::any::Any;
-use std::collections::BTreeMap;
 
 fn main() {
-    let txt = Node::Text(Text {
-        text: "World!".into(),
-    });
-    let v1 = "somev1";
-    let v2 = 11;
 
-    let x = 3u8;
-    let s = 1;
+    let old = element("div",
+        [
+            attribute("class","some-class"),
+            attribute("id", "some-id"),
+            on_event("click",|_| {
+                println!("clicked");
+            }),
+            attribute("data-id", 1111),
+            on_event("mouseover", |_| {
+                println!("i've been clicked");
+            }),
+        ],
+        [element("div", [], [text("Hello world!")])],
+    );
 
-    let div = Node::Element(Element {
-        tag: "div".into(),
-        attrs: {
-            let mut hm: BTreeMap<String, Value> = BTreeMap::new();
-            hm.insert("v1".into(), "somev1".into());
-            hm.insert("v1".into(), v1.into());
-            hm.insert("v2".into(), v2.into());
-            hm.insert("id".into(), 0.into());
-            hm
-        },
-        events: {
-            let mut hm: BTreeMap<String, Callback<Value>> = BTreeMap::new();
-            let hello = |x: Value| {
-                print!("hello  {}", x);
-            };
-            let hi = |s: Value| {
-                println!("hi: {:#?} ", s);
-            };
-            let hic: Callback<Value> = hi.into();
-            hic.emit(s);
-            hic.emit(s);
-            hic.emit(x);
-            let helloc: Callback<Value> = hello.into();
-            helloc.emit(x);
-            hm.insert("click".into(), hello.into());
-            hm.insert("mousedown".into(), hi.into());
-            hm
-        },
-        children: vec![Node::Text(Text {
-            text: "Hello".into(),
-        })],
-        namespace: None,
-    });
-
-    println!("{}", div);
-    let diff = diff::diff(&txt, &div);
-    println!("diff: {:#?}", diff);
+    let new = element("div",
+        [
+            attribute("class","some-class2"),
+            attribute("id", "some-id2"),
+            on_event("click",|_| {
+                println!("clicked2");
+            }),
+            attribute("data-id", 2222),
+            on_event("mouseover", |_| {
+                println!("i've been clicked");
+            }),
+        ],
+        [element("div", [], [text("Wazzup!")])],
+    );
+    
+    println!("old: {}", old);
+    println!("new: {}", new);
+    let patches = diff(&old, &new);
+    println!("patches: {:#?}", patches);
 }
--- crates/virtual-dom/src/lib.rs
+++ crates/virtual-dom/src/lib.rs
@@ -9,3 +9,4 @@ pub use vnode::builder;
 pub use callback::Callback;
 pub use patch::Patch;
 pub use vnode::{Element, Node, Text, Value};
+pub use diff::diff;
