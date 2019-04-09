6c1369299e9978cdebfb96cf69322dede9a0d987
Jovansonlee Cesar <ivanceras@gmail.com>
2019-04-09 15:32:16 +0800

    Refine gitlab-ci

--- test.sh
+++ test.sh
@@ -2,4 +2,10 @@
 
 cargo test --all
 
+
+# Install wasm-pack if it isn't installed yet
+if ! type wasm-pack > /dev/null; then
+    cargo install wasm-pack
+fi
+
 wasm-pack test crates/browser --firefox --headless
