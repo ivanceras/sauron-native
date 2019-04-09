c48b1074a664991c8734d45cb2009ab543db7cc6
Jovansonlee Cesar <ivanceras@gmail.com>
2019-04-09 18:42:41 +0800

    Add clock example usage

--- examples/isomorphic/client/src/lib.rs
+++ examples/isomorphic/client/src/lib.rs
@@ -5,6 +5,7 @@ use browser::html::events::*;
 use browser::html::*;
 use browser::*;
 use console_error_panic_hook;
+use js_sys::{Array, Date};
 use std::cell::{Cell, RefCell};
 use std::rc::Rc;
 use std::sync::Mutex;
@@ -15,6 +16,7 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys;
 use web_sys::console;
+use web_sys::{Document, Element, Window};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -77,6 +79,7 @@ impl Client {
             global_js.update();
         }));
 
+        setup_clock();
         let dom_updater = DomUpdater::new_replace_mount(app.view(), root_node);
         let client = Client { app, dom_updater };
         client
@@ -87,25 +90,6 @@ impl Client {
         let vdom = self.app.view();
         self.dom_updater.update(vdom);
     }
-}
-fn window() -> web_sys::Window {
-    web_sys::window().expect("no global `window` exists")
-}
-
-fn request_animation_frame(f: &Closure<FnMut()>) {
-    window()
-        .request_animation_frame(f.as_ref().unchecked_ref())
-        .expect("should register `requestAnimationFrame` OK");
-}
-
-fn document() -> web_sys::Document {
-    window()
-        .document()
-        .expect("should have a document on window")
-}
-
-fn body() -> web_sys::HtmlElement {
-    document().body().expect("document should have a body")
 }
 
 impl State {
@@ -161,6 +145,7 @@ impl App {
             [class("some-class"), id("some-id"), attr("data-id", 1)],
             [
                 div([], [text(format!("Hello world! {}", count))]),
+                div([id("current-time")], []),
                 div(
                     [],
                     [button(
@@ -225,4 +210,40 @@ impl App {
             ],
         )
     }
+}
+
+// Set up a clock on our page and update it each second to ensure it's got
+// an accurate date.
+//
+// Note the usage of `Closure` here because the closure is "long lived",
+// basically meaning it has to persist beyond the call to this one function.
+// Also of note here is the `.as_ref().unchecked_ref()` chain, which is who
+// you can extract `&Function`, what `web-sys` expects, from a `Closure`
+// which only hands you `&JsValue` via `AsRef`.
+fn setup_clock() -> Result<(), JsValue> {
+    //update_time(&current_time);
+    let a = Closure::wrap(Box::new(move || update_time()) as Box<dyn Fn()>);
+    window()
+        .set_interval_with_callback_and_timeout_and_arguments_0(a.as_ref().unchecked_ref(), 1000)?;
+    fn update_time() {
+        let current_time = document()
+            .get_element_by_id("current-time")
+            .expect("should have #current-time on the page");
+        current_time.set_inner_html(&String::from(
+            Date::new_0().to_locale_string("en-GB", &JsValue::undefined()),
+        ));
+    }
+
+    // The instances of `Closure` that we created will invalidate their
+    // corresponding JS callback whenever they're dropped, so if we were to
+    // normally return from `run` then both of our registered closures will
+    // raise exceptions when invoked.
+    //
+    // Normally we'd store these handles to later get dropped at an appropriate
+    // time but for now we want these to be global handlers so we use the
+    // `forget` method to drop them without invalidating the closure. Note that
+    // this is leaking memory in Rust, so this should be done judiciously!
+    a.forget();
+
+    Ok(())
 }
