d7260fc2eb0a4f86f41ca5e6f7060540772b71c4
Jovansonlee Cesar <ivanceras@gmail.com>
2019-04-10 22:44:53 +0800

    Add more implementation for conversion to Value
    This includes bool, arrays, and tuples

--- crates/vdom/src/vnode/value.rs
+++ crates/vdom/src/vnode/value.rs
@@ -5,6 +5,7 @@ use std::fmt;
 /// therefore a need for a storage of these intermediate value is needed
 #[derive(Debug, PartialEq, Clone)]
 pub enum Value {
+    Bool(bool),
     String(String),
     Vec(Vec<Value>),
     U8(u8),
@@ -33,6 +34,7 @@ impl Value {
 
     pub fn as_f64(&self) -> Option<f64> {
         match self {
+            Value::Bool(_) => None,
             Value::String(_v) => None,
             Value::Vec(_v) => None,
             Value::U8(v) => Some(*v as f64),
@@ -56,8 +58,16 @@ impl Value {
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Value::Bool(v) => write!(f, "{}", v),
             Value::String(v) => write!(f, "{}", v),
-            Value::Vec(v) => write!(f, "{:?}", v),
+            Value::Vec(v) => write!(
+                f,
+                "{}",
+                v.iter()
+                    .map(|i| i.to_string())
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            ),
             Value::U8(v) => write!(f, "{}", v),
             Value::U16(v) => write!(f, "{}", v),
             Value::U32(v) => write!(f, "{}", v),
@@ -82,12 +92,6 @@ impl From<&str> for Value {
     }
 }
 
-impl From<String> for Value {
-    fn from(v: String) -> Self {
-        Value::String(v)
-    }
-}
-
 macro_rules! impl_from {
     ($ty:ty => $variant:ident) => {
         impl From<$ty> for Value {
@@ -96,8 +100,35 @@ macro_rules! impl_from {
             }
         }
     };
+    (($($T:ident),*) => $($n:tt),*) => {
+        impl<$($T),*>From<($($T),*)> for Value
+            where
+                $($T: Into<Value>),*
+        {
+            fn from(v: ($($T),*)) -> Self {
+                Value::Vec(vec![$(v.$n.into()),*])
+            }
+        }
+    };
+
+    ([$T:ident;$n:tt]) => {
+        impl<T> From<[T; $n]> for Value
+        where
+            T: Into<Value> + Clone,
+        {
+            fn from(v: [T; $n]) -> Self {
+                Value::Vec(
+                    v.into_iter()
+                        .map(|i| i.to_owned().into())
+                        .collect::<Vec<Value>>(),
+                )
+            }
+        }
+    }
 }
 
+impl_from!(bool => Bool);
+impl_from!(String => String);
 impl_from!(u8 => U8);
 impl_from!(u16 => U16);
 impl_from!(u32 => U32);
@@ -112,3 +143,85 @@ impl_from!(i128 => I128);
 impl_from!(isize => Isize);
 impl_from!(f32 => F32);
 impl_from!(f64 => F64);
+
+impl_from!((T, U) => 0,1);
+impl_from!((T, U, V) => 0,1,2);
+impl_from!((T, U, V,X) => 0,1,2,3);
+impl_from!((T, U, V,X,Z) => 0,1,2,3,4);
+
+impl_from!([T; 1]);
+impl_from!([T; 2]);
+impl_from!([T; 3]);
+impl_from!([T; 4]);
+impl_from!([T; 5]);
+impl_from!([T; 6]);
+impl_from!([T; 7]);
+impl_from!([T; 8]);
+impl_from!([T; 9]);
+impl_from!([T; 10]);
+impl_from!([T; 11]);
+impl_from!([T; 12]);
+
+#[cfg(test)]
+mod tests {
+    use crate::builder::attr;
+    use crate::builder::element;
+
+    #[test]
+    fn tuple_value() {
+        let line = element("line", [attr("stroke-dasharray", (10, 20))], []);
+        let expected = "<line stroke-dasharray=\"10 20\"></line>";
+        assert_eq!(
+            format!("{}", line),
+            expected,
+            "The value in tuple should be flatten to string"
+        );
+
+        let line = element("line", [attr("transition", ("opacity", 1))], []);
+        let expected = "<line transition=\"opacity 1\"></line>";
+        assert_eq!(
+            format!("{}", line),
+            expected,
+            "The value in tuple should be flatten to string"
+        );
+
+        let line = element("line", [attr("transition", ("opacity", 1, "linear"))], []);
+        let expected = "<line transition=\"opacity 1 linear\"></line>";
+        assert_eq!(
+            format!("{}", line),
+            expected,
+            "The value in tuple should be flatten to string"
+        );
+
+        let line = element(
+            "line",
+            [attr("transition", ("opacity", 1, "linear", true))],
+            [],
+        );
+        let expected = "<line transition=\"opacity 1 linear true\"></line>";
+        assert_eq!(
+            format!("{}", line),
+            expected,
+            "The value in tuple should be flatten to string"
+        );
+    }
+
+    #[test]
+    fn array_value() {
+        let line = element("line", [attr("stroke-dasharray", [10, 20])], []);
+        let expected = "<line stroke-dasharray=\"10 20\"></line>";
+        assert_eq!(
+            format!("{}", line),
+            expected,
+            "The value in array should be flatten to string"
+        );
+
+        let line = element("line", [attr("stroke-dasharray", [10, 20, 30, 40])], []);
+        let expected = "<line stroke-dasharray=\"10 20 30 40\"></line>";
+        assert_eq!(
+            format!("{}", line),
+            expected,
+            "The value in array should be flatten to string"
+        );
+    }
+}
