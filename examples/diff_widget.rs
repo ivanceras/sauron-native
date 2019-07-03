331c603642efd209daae4dbc683ca66e384c5fa7
Jovansonlee Cesar <ivanceras@gmail.com>
2019-07-03 08:30:46 +0800

    Fix errors

--- examples/diff_widget.rs
+++ examples/diff_widget.rs
@@ -1,5 +1,6 @@
 use sauron_native::{widget::*, Node};
 use sauron_vdom::diff;
+use sauron_native::util::*;
 
 fn main() {
     let old: Node<()> = vbox(
@@ -8,19 +9,19 @@ fn main() {
             hbox(
                 vec![attr("class", "column1")],
                 vec![
-                    button(vec![value( "column1 element1")],),
-                    button(vec![value( "column1 element2")],),
-                    button(vec![value( "column1 element3")],),
-                    button(vec![value( "column1 element4")],),
-                    button(vec![value( "column1 element5")],),
-                    button(vec![value( "column1 element6")],),
+                    button(vec![value( "column1 element1")]),
+                    button(vec![value( "column1 element2")]),
+                    button(vec![value( "column1 element3")]),
+                    button(vec![value( "column1 element4")]),
+                    button(vec![value( "column1 element5")]),
+                    button(vec![value( "column1 element6")]),
                 ],
             ),
             hbox(
                 vec![attr("class", "column2")],
                 vec![button(vec![value("column2")]), button(vec![value("c2 element2")])],
             ),
-            button(vec![], &format!("Hello")),
+            button(vec![value("Hello")]),
             block("I'm a block kid!"),
             text(
                 "Hello, will this be a paragrapah\n
@@ -38,19 +39,19 @@ fn main() {
             hbox(
                 vec![attr("class", "column1")],
                 vec![
-                    button(vec![value( "Changed column1 element1")],),
-                    button(vec![value( "column1 element2")],),
-                    button(vec![value( "column1 element3")],),
-                    button(vec![value( "column1 element4")],),
-                    button(vec![value( "Changed column1 element5")],),
-                    button(vec![value( "Changed column1 element6")],),
+                    button(vec![value( "Changed column1 element1")]),
+                    button(vec![value( "column1 element2")]),
+                    button(vec![value( "column1 element3")]),
+                    button(vec![value( "column1 element4")]),
+                    button(vec![value( "Changed column1 element5")]),
+                    button(vec![value( "Changed column1 element6")]),
                 ],
             ),
             hbox(
                 vec![attr("class", "column2-changed")],
                 vec![
-                    button(vec![value("column2-changed")], ),
-                    button(vec![value("c2 element2")], ),
+                    button(vec![value("column2-changed")]),
+                    button(vec![value("c2 element2")]),
                 ],
             ),
             button(vec![value("Hello")]),
