7f605dd04d6d69c9b6944f0966ed56f2b1ff9a80
Jovansonlee Cesar <ivanceras@gmail.com>
2020-03-05 02:03:18 +0800

    Remove cross_win since cross_widget can now be run in windows

--- examples/cross_win/run_gtk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-set -v
-cargo run --features "with-gtk"
--- examples/cross_win/start_web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -v
-if wasm-pack build --target no-modules -- --features "with-html"; then
-    basic-http-server ./ -a 0.0.0.0:5000
-fi
